6df45e50c84b3bb3038cc529ce9bf6dea6879fe5
Acxel Morales <acxelmorales97@gmail.com>
2022-03-30 16:36:14 -0600

    Added auth screen and added navbar

--- src/components/auth/AuthScreen.jsx
+++ src/components/auth/AuthScreen.jsx
@@ -1,9 +1,79 @@
 import React from 'react';
 
+import './auth.css';
+
 export const AuthScreen = () => {
   return (
-    <div>
-      <h1>AuthScreen</h1>
+    <div className="container login-container">
+      <div className="row">
+        <div className="col-md-6 login-form-1">
+          <h3>Ingreso</h3>
+          <form>
+            <div className="form-group">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Correo"
+                autoComplete="fale"
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Contraseña"
+                autoComplete="false"
+              />
+            </div>
+            <div className="form-group">
+              <input type="submit" className="btnSubmit" value="Login" />
+            </div>
+          </form>
+        </div>
+
+        <div className="col-md-6 login-form-2">
+          <h3>Registro</h3>
+          <form>
+            <div className="form-group">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Nombre"
+                autoComplete="false"
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Correo"
+                autoComplete="false"
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Contraseña"
+                autoComplete="false"
+              />
+            </div>
+
+            <div className="form-group">
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Repita la contraseña"
+                autoComplete="false"
+              />
+            </div>
+
+            <div className="form-group">
+              <input type="submit" className="btnSubmit" value="Crear cuenta" />
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 
-import { AuthScreen } from '../components/auth/AuthScreen';
+import { AuthScreen }  from '../components/auth/AuthScreen';
 import { CalendarScreen } from '../components/calendar/CalendarScreen';
 
 export const AppRouter = () => {
