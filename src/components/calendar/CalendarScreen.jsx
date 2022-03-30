8bd877c8b0012894084c4db260555eb3ca92ca67
Acxel Morales <acxelmorales97@gmail.com>
2022-03-30 17:33:36 -0600

    New event component was created and listener functions were added

--- src/components/calendar/CalendarScreen.jsx
+++ src/components/calendar/CalendarScreen.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
@@ -7,6 +7,7 @@ import 'react-big-calendar/lib/css/react-big-calendar.css';
 import 'moment/locale/es';
 
 import { Navbar } from '../ui/Navbar';
+import { CalendarEvent } from './CalendarEvent';
 
 import { messages } from '../../helpers/calendar-messages-es';
 
@@ -20,10 +21,18 @@ const myEventsList = [
     end: moment().add(2, 'hours').toDate(),
     bgColor: '#fafafa',
     notes: 'Tomar cerveza 🍺',
+    user: {
+      _id: 'XXX',
+      name: 'Acxel',
+    },
   },
 ];
 
 export const CalendarScreen = () => {
+  const [lastView, setLastView] = useState(
+    localStorage.getItem('lastView') || 'month'
+  );
+
   const eventStyleGetter = (event, start, end, isSelected) => {
     const style = {
       backgroundColor: '#367CF7',
@@ -38,6 +47,19 @@ export const CalendarScreen = () => {
     };
   };
 
+  const onDoubleClick = (evt) => {
+    console.log(evt);
+  };
+
+  const onSelectEvent = (evt) => {
+    console.log(evt);
+  };
+
+  const onViewChange = (evt) => {
+    localStorage.setItem('lastView', evt);
+    setLastView(evt);
+  };
+
   return (
     <div className="calendar-screen">
       <Navbar />
@@ -49,6 +71,13 @@ export const CalendarScreen = () => {
         endAccessor="end"
         messages={messages}
         eventPropGetter={eventStyleGetter}
+        onDoubleClickEvent={onDoubleClick}
+        onSelectEvent={onSelectEvent}
+        onView={onViewChange}
+        view={lastView}
+        components={{
+          event: CalendarEvent,
+        }}
       />
     </div>
   );
