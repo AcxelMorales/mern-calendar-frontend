012c3c2772299343295b04008ef20a22354b4599
Acxel Morales <acxelmorales97@gmail.com>
2023-07-14 10:25:02 -0600

    Se agrego mejora en los estilos del modal

--- src/calendar/components/CalendarModal.jsx
+++ src/calendar/components/CalendarModal.jsx
@@ -8,9 +8,11 @@ import Modal from 'react-modal';
 
 import DatePicker, { registerLocale } from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-
 import es from 'date-fns/locale/es';
+
 import { useCalendarStore, useUiStore } from '../../hooks';
+
+import './Modal.css';
 
 registerLocale('es', es);
 
@@ -100,11 +102,11 @@ export const CalendarModal = () => {
       overlayClassName="modal-fondo"
       closeTimeoutMS={200}
     >
-      <h1> Nuevo evento </h1>
+      <h1 className="title-modal"> Nuevo evento </h1>
       <hr />
       <form className="container" onSubmit={onSubmit}>
-        <div className="form-group mb-2">
-          <label>Fecha y hora inicio</label>
+        <div className="form-group mb-3">
+          <label className="label-date">Fecha y hora inicio</label>
           <DatePicker
             selected={formValues.start}
             onChange={(event) => onDateChanged(event, 'start')}
@@ -116,8 +118,8 @@ export const CalendarModal = () => {
           />
         </div>
 
-        <div className="form-group mb-2">
-          <label>Fecha y hora fin</label>
+        <div className="form-group mb-3">
+          <label className="label-date">Fecha y hora fin</label>
           <DatePicker
             minDate={formValues.start}
             selected={formValues.end}
@@ -130,8 +132,7 @@ export const CalendarModal = () => {
           />
         </div>
 
-        <hr />
-        <div className="form-group mb-2">
+        <div className="form-group mb-3">
           <label>Titulo y notas</label>
           <input
             type="text"
@@ -147,7 +148,7 @@ export const CalendarModal = () => {
           </small>
         </div>
 
-        <div className="form-group mb-2">
+        <div className="form-group mb-4">
           <textarea
             type="text"
             className="form-control"
